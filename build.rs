ed4c24f39655c5f595a9147aec8b524b50e4c965
adakite1 <adakite.a@gmail.com>
2023-09-16 05:26:53 +0800

    Move bindings for `adpcm-xq-nds` to its own crate (3)

--- build.rs
+++ build.rs
@@ -2,19 +2,11 @@ use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     cc::Build::new()
-        .file("adpcm-xq-nds/adpcm-xq.c")
-        .file("adpcm-xq-nds/adpcm-lib.c")
-        .compile("adpcm-xq-nds");
-    cc::Build::new()
         .cpp(true)
         .include(PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?).join("r8brain-free-src"))
         .file("r8brain-free-src/r8bbase.cpp")
         .file("src/resampler-extern.cpp")
         .compile("resampler");
-    // cc::Build::new()
-    //     .include(PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?).join("adpcm-xq"))
-    //     .file("src/lib.c")
-    //     .compile("dse-dsp-sys");
     println!("cargo:rerun-if-changed=src/resampler-extern.cpp");
     println!("cargo:rerun-if-changed=src/lib.c");
     Ok(())
